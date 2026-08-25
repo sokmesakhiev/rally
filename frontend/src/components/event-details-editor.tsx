e95bd2e5f6f0d506a3cf9e47ed3c9587f0b64701
Sokmesa Khiev <sokmesa@gmail.com>
2026-08-25 14:28:16 +0700

    Add account verification (#273)

--- frontend/src/components/event-details-editor.tsx
+++ frontend/src/components/event-details-editor.tsx
@@ -357,10 +357,7 @@ export function EventDetailsEditor({
             types={types}
             onTypesChange={handleTypesChange}
             eventPriceCents={isPaid ? toCents(price) : 0}
-<<<<<<< HEAD
             allowPricing={canPrice}
-=======
->>>>>>> 0f06714 (Add editable to event type (#269))
           />
         )}
       </div>
